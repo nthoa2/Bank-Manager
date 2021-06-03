fbe44dc35aba580510898aa0b6851877ac9ea2a4
Jung Nguyen <nthoa2.lhp.nh@gmail.com>
2021-06-03 09:59:17 +0700

    pre merge

---
+++ Projects/BankManager/src/Main/main.java
@@ -0,0 +1,18 @@
+package Main;
+
+
+import Views.Main;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        main.run();
+    }
+}
